2565e2eef1f6d32338ccd52dbfc04cc43a619deb
SelvendhiranNagaraj <selvendhiran.nagaraj@gmail.com>
2022-04-06 18:33:51 -0500

    Add test

--- booking/src/main/java/com/freight/booking/BookingMain.java
+++ booking/src/main/java/com/freight/booking/BookingMain.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingMain {
     public static void main(String[] args)
     {
-        System.out.println("test");
         SpringApplication.run(BookingMain.class,args);
     }
 }
